5383dae687e4f7760621172de10cae775f2cd99d
songlinhou <307977586@qq.com>
2019-09-24 02:33:55 -0400

    update text

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ chmod +x dbh
 chmod +x dbp
 chmod +x set_path.py
 python set_path.py # set path
+source ~/.bash_profile
 
 echo 
 echo -e "\e[32m ALL DONE! Now try use the following commands anywhere\e[0m"
